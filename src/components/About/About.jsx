511bdaa37f3474dfce41afbb81a4f783e1ddcaec
Yash Kathait <91113846+yashkatha@users.noreply.github.com>
2025-09-08 20:01:34 +0530

    new change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
 import profileImage from '../../assets/LogoID.jpg';
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
